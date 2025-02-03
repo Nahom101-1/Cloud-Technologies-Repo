9824d7d1aeb67993a2d67c967e2a4831696ba342
Nahom Berhane <nahomberhane@Nahoms-MacBook-Pro.local>
2025-02-03 12:41:25 +0100

    Updated main.go files for JSON encoding notes  and TODO-GO youtube tutorial

--- notes/Json Encoding/main.go
+++ notes/Json Encoding/main.go
@@ -1,1 +1,46 @@
-package Json_Encoding
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Todo struct {
+	UserID    int    `json:"userId"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func main() {
+	url := "https://jsonplaceholder.typicode.com/todos/2"
+	response, err := http.Get(url)
+	if err != nil {
+		//error and then OS.exit()
+		log.Fatal(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(response.Body)
+	if response.StatusCode == http.StatusOK {
+		todoItem := Todo{}
+		decoder := json.NewDecoder(response.Body)
+		/*decoder.DisallowUnknownFields()*/ //prevents unknown fields in JSON
+		err := decoder.Decode(&todoItem)    // decode the JSON into the struct
+		if err != nil {
+			log.Fatal(err)
+		}
+		/*fmt.Printf("Data from API: %+v\n", todoItem)*/
+		//COvert back to JSON
+		todo, _ := json.MarshalIndent(todoItem, "", "\t")
+
+		fmt.Println(string((todo)))
+
+	}
+}
--- notes/TODO-GO/main.go
+++ notes/TODO-GO/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"item"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Buy milk", Completed: false},
+	{ID: "2", Item: "Buy eggs", Completed: false},
+	{ID: "3", Item: "Buy bread", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+
+}
+
+func addTodo(context *gin.Context) {
+	var newTodo todo
+
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
+	router.Run("localhost:8080")
+
+}
